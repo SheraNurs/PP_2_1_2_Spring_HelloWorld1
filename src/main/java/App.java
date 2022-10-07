90580be499fe02d64e9f863b1e25fa220b618b5d
Nursultan <sheralievnursultan1@gmail.com>
2022-10-07 16:09:35 +0600

    Add Cat.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+        System.out.println(cat.getMessage());
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
     }
 }
